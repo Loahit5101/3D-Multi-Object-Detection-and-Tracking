64051b0aeef1b998288f0c6d7ce264794f7b2ab6
Loahit Krishnamurthy <loahit5101@gmail.com>
2024-03-09 13:26:08 -0500

    Setup parsing and Filtering Pipeline
    1. Created a ros package to read and publish KITTI point clouds.
    2. Created objects to perform point cloud Filtering

---
+++ scripts/base_filter.h
@@ -0,0 +1,19 @@
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <algorithm>
+
+class BaseFilter{
+
+ public:
+  BaseFilter() = default;
+ 
+  virtual ~BaseFilter() =default;
+ 
+  virtual void apply_filter(pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud) = 0;
+
+};
+
+
---
+++ scripts/main.cpp
@@ -0,0 +1,91 @@
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <boost/filesystem.hpp>
+#include <algorithm>
+
+#include "filters.h"
+
+namespace fs = boost::filesystem;
+
+bool compareFiles(const fs::directory_entry& entry1, const fs::directory_entry& entry2)
+{
+    return entry1.path().filename().string() < entry2.path().filename().string();
+}
+
+pcl::PointCloud<pcl::PointXYZI>::Ptr loadPointCloud(const std::string& file_path)
+{
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+    if (pcl::io::loadPCDFile<pcl::PointXYZI>(file_path, *cloud) == -1)
+    {
+        ROS_ERROR("Could not read file: %s", file_path.c_str());
+        return nullptr;
+    }
+    return cloud;
+}
+
+void publishPointCloud(ros::Publisher& pub, const pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud)
+{
+    if (cloud)
+    {
+        sensor_msgs::PointCloud2 pcl_msg;
+        pcl::toROSMsg(*cloud, pcl_msg);
+        pcl_msg.header.frame_id = "velodyne";
+        pub.publish(pcl_msg);
+    }
+}
+
+std::vector<fs::directory_entry> createFileList(const std::string& input_folder_path){
+
+    std::vector<fs::directory_entry> file_list;
+    for (const auto& entry : fs::directory_iterator(input_folder_path))
+    {
+        if (entry.path().extension() == ".pcd")
+        {
+            file_list.push_back(entry);
+        }
+    }
+
+    std::sort(file_list.begin(), file_list.end(), compareFiles);
+
+    return file_list;
+}
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "point_cloud_publisher");
+    ros::NodeHandle nh;
+    ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud2>("point_cloud_topic", 1);
+    ros::Rate loop_rate(10);
+
+    std::string input_folder_path = "/home/loahit/Downloads/projects/perception_project/0020/pcd_files/";
+
+    std::vector<fs::directory_entry> file_list;
+    file_list = createFileList(input_folder_path);
+
+    // Loop through poinclouds
+    for (const auto& entry : file_list)
+    {
+        std::string input_file_path = entry.path().string();
+
+        pcl::PointCloud<pcl::PointXYZI>::Ptr cloud = loadPointCloud(input_file_path);
+
+        StatisticalOutlierRemoval stat(50,1.0);
+        DownsampleFilter Downsample(0.1f);
+        CropBoxFilter crop(Eigen::Vector4f (-10, -10, -10, 1),Eigen::Vector4f (10, 10, 10, 1));
+        
+
+        Downsample.apply_filter(cloud);
+        stat.apply_filter(cloud);
+        crop.apply_filter(cloud);
+
+        publishPointCloud(pub, cloud);
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
